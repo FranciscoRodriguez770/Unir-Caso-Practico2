61a77ffd12a808da122e699458e31c358c019e9a
FranciscoRodriguez770 <96494336+FranciscoRodriguez770@users.noreply.github.com>
2022-03-04 18:15:23 +0000

    Delete credentials.tf

--- Terraform/credentials.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "b7a988b6-bf36-4262-9264-885c2c07cf93"
-  client_id       = "1cdb0e25-a07f-48cf-ad35-03d727f99bfc"
-  client_secret   = "pP5grz.W8Ib-fX_YhSHl9GYyB2T9BZfXT2"
-  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-}
